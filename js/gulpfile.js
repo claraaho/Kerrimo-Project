398a128a76d43cfdb94efc7f3125ad51f951e603
Edward <edwardlanto@hotmail.com>
2017-02-09 21:25:41 -0800

    initial commit

---
+++ js/gulpfile.js
@@ -0,0 +1,8 @@
+// Require Gulp first!
+var gulp = require('gulp');
+// This is a very basic Gulp task,
+// with a name and some code to run
+// when this task is called:
+gulp.task('default', function() {
+   alert('Hello world');
+});
